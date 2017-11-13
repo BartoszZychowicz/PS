48b4009a18bd62417de401a255031d323ac75c2c
BartoszZychowicz <zychowiczbartosz@gmail.com>
2017-11-13 19:34:42 +0100

    zmiana nazwy

---
+++ lab1/lab1/helloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!"); 
+            Console.ReadLine();            
+        }
+    }
+}
