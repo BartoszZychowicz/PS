ab0c4e88dc5e364d4e740dfa85e0e0cb7dfa2b3f
BartoszZychowicz <zychowiczbartosz@gmail.com>
2017-11-13 22:26:39 +0100

    Lab 1 - poprawione wersje plików z odpowiednimi nazwami

---
+++ lab1helloWorld/lab1helloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab1helloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
