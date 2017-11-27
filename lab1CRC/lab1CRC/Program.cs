4fe2af509ab010f3cc7b8e7a290910d74fbadec8
BartoszZychowicz <zychowiczbartosz@gmail.com>
2017-11-27 22:45:11 +0100

    Pierwsza dzialajaca wersja, dodano komentarze

--- lab1CRC/lab1CRC/Program.cs
+++ lab1CRC/lab1CRC/Program.cs
@@ -12,26 +12,27 @@ namespace lab1CRC
             int rozmiar_danych = 8;
             int stopien_CRC = 3;
             int dlugosc_wielomianu = stopien_CRC + 1;
-            wielomian = wielomian << (rozmiar_danych - (stopien_CRC + 1));
-            int crc = 0;
-            int temp;
+            wielomian = wielomian << (rozmiar_danych - (stopien_CRC + 1));    //przesuniecie wielomianu
+            int crc;    //inicjalizacja sumy kontrolnej
+            int temp;    //zmienna tymczasowa
+            
             Console.Write("Wartosc:" + dane + "\nBinarnie:" );
-            for(int i=(rozmiar_danych-1);i>=0;i--)
+            for(int i=(rozmiar_danych-1);i>=0;i--)        //wypisanie postaci binarnej danych
             {
                 temp = (dane>>i)&1;
                 Console.Write(temp);
             }
-            crc = dane;
+            crc = dane;    //pierwsza wartosc crc rowna danym wejsciowym
             for (int i = 0; i < rozmiar_danych; i++)
             {
-                if ((crc & (1<<(rozmiar_danych-1))) != 0)
+                if ((crc & (1<<(rozmiar_danych-1))) != 0)    //jezeli MSB danych to 1
                 {
-                    crc = crc ^ wielomian;
+                    crc = crc ^ wielomian;    //XOR wielomianu i danych
                 }
-                crc = crc << 1;
+                crc = crc << 1;    //przesuniecie w celu XORowanie kolejnego bitu
             }
             Console.Write("\nSuma kontrolna:");
-            Console.Write(crc>>(rozmiar_danych-stopien_CRC));
+            Console.Write(crc>>(rozmiar_danych-stopien_CRC));    //tylko pozycje znaczace sumy kontrolnej
 
         }
     }
